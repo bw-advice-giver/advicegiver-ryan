2005aae4573fc0ce5c265be8f5753b44038bd83b
donutwizard666 <thebeardedwarrior@icloud.com>
2019-09-25 16:03:38 -0700

    pair programmed with ben and learned some javascript

---
+++ js/handlerfunctions.js
@@ -0,0 +1,13 @@
+// function onSubmit(event) {
+//     event.preventDefault();
+//     return state {
+//         firstname: '',
+//         lastname: '',
+//         emailaddress: '',
+
+//     }
+// }
+
+// Pair programmed with Benjamin Griffin. 
+//Was given and introduction to State and how it relates to forms.
+//Typed out an example function above and we walked through how this function would work and save the user's data//
